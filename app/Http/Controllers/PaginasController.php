602d4e1419e4f2100d927d0f1b88cf344e53153f
ricchustorm <ricchustorm@gmail.com>
2019-10-15 13:48:37 +0200

    Initial commit

---
+++ app/Http/Controllers/PaginasController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PaginasController extends Controller{
+    
+    public function inicio (){
+        
+        return view ('welcome');
+    }
+    
+        public function notices(){
+        
+        return view ('notices');
+    }
+    
+   
+    
+    public function articles(){
+        
+        return view ('articles');
+    }
+    
+    
+    
+    public function gotw(){
+        
+        return view ('gotw');
+    }
+    
+ 
+    
+    public function contactus(){
+        
+        return view ('contactus');
+    }
+    
+    
+    
+};
+
+
+    
+
+   
